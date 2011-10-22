a325959948da6e283fa053a8f90eb7772584b1fb
nils <nils@34ebc366-c3a9-4b3c-9f84-69acf7962910>
2011-10-22 16:55:27 +0000

    changed to GPLv3
    git-svn-id: http://cpunk.de/svn/src/gpg4usb/trunk@564 34ebc366-c3a9-4b3c-9f84-69acf7962910

--- verifynotification.h
+++ verifynotification.h
@@ -3,20 +3,20 @@
  *
  *      Copyright 2008 gpg4usb-team <gpg4usb@cpunk.de>
  *
- *      This program is free software; you can redistribute it and/or modify
+ *      This file is part of gpg4usb.
+ *
+ *      Gpg4usb is free software: you can redistribute it and/or modify
  *      it under the terms of the GNU General Public License as published by
- *      the Free Software Foundation; either version 2 of the License, or
+ *      the Free Software Foundation, either version 3 of the License, or
  *      (at your option) any later version.
  *
- *      This program is distributed in the hope that it will be useful,
+ *      Gpg4usb is distributed in the hope that it will be useful,
  *      but WITHOUT ANY WARRANTY; without even the implied warranty of
  *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *      GNU General Public License for more details.
  *
  *      You should have received a copy of the GNU General Public License
- *      along with this program; if not, write to the Free Software
- *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- *      MA 02110-1301, USA.
+ *      along with gpg4usb.  If not, see <http://www.gnu.org/licenses/>
  */
 
 #ifndef __VERIFYNOTIFICATION_H__
