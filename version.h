0b3703fa03d4962c7b45286be4b76f74653eb289
ubbo <ubbo@34ebc366-c3a9-4b3c-9f84-69acf7962910>
2013-03-29 02:55:00 +0000

    add default version.h
    git-svn-id: http://cpunk.de/svn/src/gpg4usb/trunk@1031 34ebc366-c3a9-4b3c-9f84-69acf7962910

---
+++ version.h
@@ -0,0 +1,12 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+namespace Version
+{
+	static const int MAJOR = 0;
+	static const int MINOR = 4;
+	static const int REVISION = 0;
+	static const int BUILD = 0;
+}
+
+#endif // VERSION_H
