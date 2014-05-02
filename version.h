bc13a6761d8a5d605041424a8ef86f9fe1c98601
ubbo <ubbo@34ebc366-c3a9-4b3c-9f84-69acf7962910>
2014-05-02 12:29:27 +0000

    removed gpgme libs from build, fixed qmake-linux.sh
    git-svn-id: http://cpunk.de/svn/src/gpg4usb/trunk@1105 34ebc366-c3a9-4b3c-9f84-69acf7962910

--- version.h
+++ version.h
@@ -5,8 +5,8 @@ namespace Version
 {
 	static const int MAJOR = 0;
 	static const int MINOR = 4;
-	static const int REVISION = 1032;
-	static const int BUILD = 19088;
+	static const int REVISION = 1104;
+	static const int BUILD = 20121;
 }
 
 #endif // VERSION_H
